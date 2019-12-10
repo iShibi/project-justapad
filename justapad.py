a527322e823cc78c6024e096f11c5bdd07992b5e
Shubham Parihar <51517103+shibi391@users.noreply.github.com>
2019-12-10 16:17:14 +0530

    Added some new features.
    1) Now the focus is by default on text area. No need to click on it to start writing.
    2) Added shortcut key bindings for "new file", "open...", "save", "undo", and "redo" options.
    3) Added options for cut, copy, and paste in the edit menu.
    4) The text editor will ask whether you want to save the changes or not when you exit or create a new file.

--- justapad.py
+++ justapad.py
@@ -9,44 +9,68 @@ root.title("justapad")
 root.iconbitmap("icon.ico")
 
 #**** Functions ****
-def new():
+def new(event=""):
+    if len(text_area.get("1.0", END+"-1c")) != 0:
+        if messagebox.askyesno("Save?", "Do you wish to save?"):
+            if save():
+                text_area.delete(1.0, END)
+        else:
+            text_area.delete(1.0, END)
+def open(event=""):
     if len(text_area.get("1.0", END+"-1c")) != 0:
         if messagebox.askyesno("Save?", "Do you wish to save?"):
             save()
+        file = askopenfile()
+        if file != None:
             text_area.delete(1.0, END)
-        else:
-            text_area.delete(1.0, END)
-def open():
-    file = askopenfile()
-    text_area.delete(1.0, END)
-    if file != None:
-        content = file.read()
-        text_area.insert(1.0, content)
-        file.close()
+            content = file.read()
+            text_area.insert(1.0, content)
+            file.close()
+    else:
+        file = askopenfile()
+        #text_area.delete(1.0, END)
+        if file != None:
+            content = file.read()
+            text_area.insert(1.0, content)
+            file.close()
 
-def save():
+def save(event=""):
+    saved = False
     list_of_filetypes = [("Text Documents", "*.txt"), ("All Files", "*.*")]
     file = asksaveasfile(filetypes=list_of_filetypes, defaultextension=list_of_filetypes)
     if file != None:
         text = text_area.get(1.0, END+"-1c")
         file.write(text)
         file.close()
+        saved = True
+    return saved
 
 def exit():
+    if len(text_area.get("1.0", END+"-1c")) != 0:
+        if messagebox.askyesno("Save?", "Do you wish to save?"):
+            save()
     root.quit()
 
 def about():
-    description = messagebox.showinfo("About justapad", "justapad\nv1.0\nMade by Shubham Parihar.")
+    description = messagebox.showinfo("About justapad", "justapad\nv1.2\nA text editor made by Shubham Parihar.")
 
-def undo():
+def undo(event=""):
     if len(text_area.get("1.0", END + "-1c")) != 0:
         text_area.edit_undo()
 
-def redo():
+def redo(event=""):
     try:
         text_area.edit_redo()
     except:
         pass
+def cut():
+    text_area.focus_get().event_generate('<<Cut>>')
+
+def copy():
+    text_area.focus_get().event_generate('<<Copy>>')
+
+def paste():
+    text_area.focus_get().event_generate('<<Paste>>')
 
 #**** Scroll Bar ****
 scrollbar = Scrollbar(root)
@@ -66,19 +90,32 @@ edit = Menu(menubar, tearoff=0)
 help = Menu(menubar, tearoff=0)
 
 menubar.add_cascade(label="File", menu=file)
-file.add_command(label="New", command=new)
-file.add_command(label="Open...", command=open)
-file.add_command(label="Save", command=save)
+file.add_command(label="New                             Ctrl+N", command=new)
+file.add_command(label="Open...                        Ctrl+O", command=open)
+file.add_command(label="Save                             Ctrl+S", command=save)
 file.add_separator()
 file.add_command(label="Exit", command=exit)
 
 menubar.add_cascade(label="Edit", menu=edit)
-edit.add_command(label="Undo", command=undo)
-edit.add_command(label="Redo", command=redo)
+edit.add_command(label="Undo                         Ctrl+Z", command=undo)
+edit.add_command(label="Redo                          Ctrl+Y", command=redo)
+edit.add_separator()
+edit.add_command(label="Cut                             Ctrl+X", command=cut)
+edit.add_command(label="Copy                          Ctrl+C", command=copy)
+edit.add_command(label="Paste                          Ctrl+V", command=paste)
 
 menubar.add_cascade(label="Help", menu=help)
 help.add_command(label="About justapad", command=about)
 
+#**** Shortcut Keys ****
+root.bind('<Control-n>', new)
+root.bind('<Control-o>', open)
+root.bind('<Control-s>', save)
+root.bind('<Control-z>', undo)
+root.bind('<Control-y>', redo)
+
 #**** Main Loop ****
 root.geometry("500x500+450+150")
+text_area.focus()
+root.protocol('WM_DELETE_WINDOW', exit)
 root.mainloop()
