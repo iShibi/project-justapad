41c2cf7a9bcfdce46d8b7f2dedff7fe243efaa04
Shubham Parihar <51517103+shibi391@users.noreply.github.com>
2019-12-11 15:31:17 +0530

    Add files via upload

--- justapad.py
+++ justapad.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.filedialog import asksaveasfile
 from tkinter.filedialog import askopenfile
 from tkinter import messagebox
+import tkinter.font as tkFont
 
 #**** Window Settings ****
 root = Tk()
@@ -16,6 +17,7 @@ def new(event=""):
                 text_area.delete(1.0, END)
         else:
             text_area.delete(1.0, END)
+
 def open(event=""):
     if len(text_area.get("1.0", END+"-1c")) != 0:
         if messagebox.askyesno("Save?", "Do you wish to save?"):
@@ -72,12 +74,30 @@ def copy():
 def paste():
     text_area.focus_get().event_generate('<<Paste>>')
 
+def increase_font_size():
+    if custom_font['size'] < 100:
+        custom_font['size'] += 1
+
+def decrease_font_size():
+    if custom_font['size'] > 4:
+        custom_font['size'] -= 1
+
+def togle_font(event):
+    if event.keysym == 'plus':
+        if custom_font['size'] < 100:
+            custom_font['size'] += 1
+    elif event.keysym == 'minus':
+        if custom_font['size'] > 4:
+            custom_font['size'] -= 1
+
 #**** Scroll Bar ****
 scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT, fill=Y)
 
 #**** Text Area ****
+custom_font = tkFont.Font(family="Times New Roman", size=12)
 text_area = Text(root, yscrollcommand=scrollbar.set, height=500, width=500, undo=TRUE)
+text_area.configure(font=custom_font)
 text_area.pack()
 scrollbar.config(command=text_area.yview)
 
@@ -87,10 +107,12 @@ root.config(menu=menubar)
 
 file = Menu(menubar, tearoff=0)
 edit = Menu(menubar, tearoff=0)
+format = Menu(menubar, tearoff=0)
 help = Menu(menubar, tearoff=0)
 
 menubar.add_cascade(label="File", menu=file)
 file.add_command(label="New                             Ctrl+N", command=new)
+#file.add_command(label="New Window")
 file.add_command(label="Open...                        Ctrl+O", command=open)
 file.add_command(label="Save                             Ctrl+S", command=save)
 file.add_separator()
@@ -104,6 +126,10 @@ edit.add_command(label="Cut                             Ctrl+X", command=cut)
 edit.add_command(label="Copy                          Ctrl+C", command=copy)
 edit.add_command(label="Paste                          Ctrl+V", command=paste)
 
+menubar.add_cascade(label="Format", menu=format)
+format.add_command(label="Increase Font Size", command=increase_font_size)
+format.add_command(label="Decrease Font Size", command=decrease_font_size)
+
 menubar.add_cascade(label="Help", menu=help)
 help.add_command(label="About justapad", command=about)
 
@@ -113,6 +139,8 @@ root.bind('<Control-o>', open)
 root.bind('<Control-s>', save)
 root.bind('<Control-z>', undo)
 root.bind('<Control-y>', redo)
+root.bind('<Control-plus>', togle_font)
+root.bind('<Control-minus>', togle_font)
 
 #**** Main Loop ****
 root.geometry("500x500+450+150")
